4a54c42399fe7b1b1dc224eaaf9698302db7adbf
Vishal Sharma <61149706+vks16@users.noreply.github.com>
2020-09-23 14:13:00 +0530

    Update square_root.go

--- square_root.go
+++ square_root.go
@@ -1,7 +1,7 @@
 package math 
 
 // Function of Square root to calculate the square root of the function 
-func Square_root(val int) float64 {
+func Square_root(val float64) float64 {
     z := 1.0
     if val == 0 {
         return 0.0
