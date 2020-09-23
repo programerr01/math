a270060a25fb5f5d8a2664bd08d7cc3f92960b68
Vishal Sharma <61149706+vks16@users.noreply.github.com>
2020-09-23 14:14:04 +0530

    Update square_root_test.go

--- square_root_test.go
+++ square_root_test.go
@@ -2,11 +2,12 @@ package math
 import "testing"
 
 func Test_square_root(t *testing.T) {
-    sample := map[int]float64 {
+    sample := map[float64]float64 {
     0: 0.0,
     1: 1.0,
     4:2.0 ,
     9: 3.0,
+        16.0: 4.0,
 
     }
     for i , v := range sample {
